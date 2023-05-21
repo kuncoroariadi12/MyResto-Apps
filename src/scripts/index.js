b92c4f058547959c93b9d77e392d3128336f4427
kuncoroariadi12 <119470883+kuncoroariadi12@users.noreply.github.com>
2023-05-21 18:04:38 +0700

    Update index.js
    Adding import and DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
+import main from './main.js';
 
-console.log('Hello Coders! :)');
+document.addEventListener('DOMContentLoaded', main);
